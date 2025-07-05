170c2e90f47da77ed01d2185d7496dc7e3010aaa
longdo <dolong2310@gmail.com>
2025-07-05 17:45:02 +0700

    update: setup trpc & call test react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-const Home = () => {
-  return <div>HELLO LONGDOO</div>;
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import Client from "./client";
+
+const Home = async () => {
+  const queryClient = getQueryClient();
+  void await queryClient.prefetchQuery(
+    trpc.helloAi.queryOptions({ text: "Longdoo" })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Home;
