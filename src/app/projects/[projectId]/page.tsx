dc5df21fb1f442b00f4cb57c2bb42362a7281a29
longdo <dolong2310@gmail.com>
2025-07-08 16:04:21 +0700

    add: create messages ui & intergrate api messages

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import ProjectView from "@/modules/projects/ui/views/ProjectView";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import React, { Suspense } from "react";
 
 type Props = {
   params: Promise<{
@@ -9,7 +12,25 @@ type Props = {
 const Project = async ({ params }: Props) => {
   const { projectId } = await params;
 
-  return <div>Project: {projectId}</div>;
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(
+    trpc.messages.getMany.queryOptions({
+      projectId,
+    })
+  );
+  await queryClient.prefetchQuery(
+    trpc.projects.getOne.queryOptions({
+      id: projectId,
+    })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProjectView projectId={projectId} />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Project;
---
+++ src/modules/projects/ui/views/ProjectView.tsx
@@ -0,0 +1,42 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Suspense } from "react";
+import MessagesContainer from "../components/MessagesContainer";
+
+type Props = {
+  projectId: string;
+};
+
+const ProjectView = ({ projectId }: Props) => {
+  // const trpc = useTRPC();
+  // const { data: project } = useSuspenseQuery(
+  //   trpc.projects.getOne.queryOptions({
+  //     id: projectId,
+  //   })
+  // );
+
+  return (
+    <div className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+          defaultSize={35}
+          minSize={20}
+          className="flex flex-col min-h-0"
+        >
+          <Suspense fallback={<div>Loading messages...</div>}>
+            <MessagesContainer projectId={projectId} />
+          </Suspense>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65} minSize={50}>
+          TODO: Preview
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
+
+export default ProjectView;
