2112606219a9489b5bc49c0c2ec3f0faf81112dd
longdo <dolong2310@gmail.com>
2025-07-09 17:41:56 +0700

    update: change new theme & add new variant tertiary for button

--- src/modules/projects/ui/views/ProjectView.tsx
+++ src/modules/projects/ui/views/ProjectView.tsx
@@ -65,7 +65,7 @@ const ProjectView = ({ projectId }: Props) => {
               </TabsList>
 
               <div className="ml-auto flex items-center gap-x-2">
-                <Button asChild size="sm" variant="default">
+                <Button asChild size="sm" variant="tertiary">
                   <Link href="/pricing">
                     <CrownIcon /> Upgrade
                   </Link>
