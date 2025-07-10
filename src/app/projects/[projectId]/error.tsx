b683c88a31775a886cdd3f23b4cfee3359892e54
longdo <dolong2310@gmail.com>
2025-07-10 18:17:55 +0700

    add: react error boundary & update: improve error handling

---
+++ src/app/projects/[projectId]/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Project Error</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
